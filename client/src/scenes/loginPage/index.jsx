3d0a9156165c0774c16bd99d013167bddceec569
Prathap-4903 <prathap4903@gmail.com>
2024-10-29 23:05:24 +0530

    Updated Login

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to SoulSync, Make Your Soul To Sync!
         </Typography>
         <Form />
       </Box>
