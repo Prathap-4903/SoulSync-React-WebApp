351169969fd703b99ab620d59763ea9f28978d24
Prathap <prathap4903@gmail.com>
2024-12-29 22:35:22 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,7 +19,6 @@ export const createPost = async (req, res) => {
       comments: [],
     });
     await newPost.save();
-
     const post = await Post.find();
     res.status(201).json(post);
   } catch (err) {
